d61c89580316c3f4ede5e150844c0568c644fdd8
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2022-04-15 18:36:33 +0200

    EVERYTHING MAPPED NIGGA + BUTTONS

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -18,6 +18,8 @@ async function loadEvent() {
 
     countriesSection.insertAdjacentHTML('beforebegin', `
     <h1>Countries: </h1>
+    <button class="flag-add">Show flags as background</button>
+    <button class="flag-remove">Remove the flags from tha background</button>
     `);
 
     const countryComponent = countryName => {
@@ -58,40 +60,75 @@ async function loadEvent() {
     //ARRAY.MAP FOR CIKLUS HELYETT
     // [...countriesJSON].map(country => countriesSection.insertAdjacentHTML('beforeend', countryComponent(country.name.common)));
 
-    for (let i = 0; i < countriesJSON.length; i++) {
-        // countriesSection.insertAdjacentHTML('beforeend', `${ countriesJSON[i].name.common } `);
-        if (!countriesJSON[i].capital) {
-            countriesSection.insertAdjacentHTML('beforeend',
-                `<div class="countrycont ${i}">
-                ${countryComponent(countriesJSON[i].name.common)}
-                \n${regionComponent(countriesJSON[i].region)}
-                \n${capitalComponent("None")}
-                \n${independentComponent(countriesJSON[i].independent)}
-                \n${flagComponent(countriesJSON[i].flag)}
-             </div>}`)
-                ;
-            // countriesSection.insertAdjacentHTML('beforeend', independentComponent(countriesJSON[i].independent));
-            // countriesSection.insertAdjacentHTML('beforeend', flagComponent(countriesJSON[i].flag));
-        } else {
-            countriesSection.insertAdjacentHTML('beforeend',
-                `<div class="countrycont ${i}">
-                ${countryComponent(countriesJSON[i].name.common)}
-                \n${regionComponent(countriesJSON[i].region)}
-                \n${capitalComponent(countriesJSON[i].capital[0])}
-                \n${independentComponent(countriesJSON[i].independent)}
-                \n${flagComponent(countriesJSON[i].flag)}
-                </div>`);
-            // countriesSection.insertAdjacentHTML('beforeend', capitalComponent(countriesJSON[i].capital[0]));
-            // countriesSection.insertAdjacentHTML('beforeend', independentComponent(countriesJSON[i].independent));
-            // countriesSection.insertAdjacentHTML('beforeend', flagComponent(countriesJSON[i].flag));
-            console.log(Object.values(countriesJSON[i].flags)[0])
-        };
+    countriesJSON.map(countries => !countries.capital ? countriesSection.insertAdjacentHTML('beforeend',
+        `<div class="countrycont">
+            ${countryComponent(countries.name.common)}
+            \n${regionComponent(countries.region)}
+            \n${capitalComponent("None")}
+            \n${independentComponent(countries.independent)}
+            \n${flagComponent(countries.flag)}
+        </div>}`)
+        : countriesSection.insertAdjacentHTML('beforeend',
+            `<div class="countrycont">
+            ${countryComponent(countries.name.common)}
+            \n${regionComponent(countries.region)}
+            \n${capitalComponent(countries.capital[0])}
+            \n${independentComponent(countries.independent)}
+            \n${flagComponent(countries.flag)}
+        </div>`));
+
+    //     // for (let i = 0; i < countriesJSON.length; i++) {
+    //     //     // countriesSection.insertAdjacentHTML('beforeend', `${ countriesJSON[i].name.common } `);
+    //     //     if (!countriesJSON[i].capital) {
+    //     //         countriesSection.insertAdjacentHTML('beforeend',
+    //     //             `<div class="countrycont ${i}">
+    //     //             ${countryComponent(countriesJSON[i].name.common)}
+    //     //             \n${regionComponent(countriesJSON[i].region)}
+    //     //             \n${capitalComponent("None")}
+    //     //             \n${independentComponent(countriesJSON[i].independent)}
+    //     //             \n${flagComponent(countriesJSON[i].flag)}
+    //     //          </div>}`)
+    //     //             ;
+    //     //         // countriesSection.insertAdjacentHTML('beforeend', independentComponent(countriesJSON[i].independent));
+    //     //         // countriesSection.insertAdjacentHTML('beforeend', flagComponent(countriesJSON[i].flag));
+    //     //     } else {
+    //     //         countriesSection.insertAdjacentHTML('beforeend',
+    //     //             `<div class="countrycont ${i}">
+    //     //             ${countryComponent(countriesJSON[i].name.common)}
+    //     //             \n${regionComponent(countriesJSON[i].region)}
+    //     //             \n${capitalComponent(countriesJSON[i].capital[0])}
+    //     //             \n${independentComponent(countriesJSON[i].independent)}
+    //     //             \n${flagComponent(countriesJSON[i].flag)}
+    //     //             </div>`);
+    //     // countriesSection.insertAdjacentHTML('beforeend', capitalComponent(countriesJSON[i].capital[0]));
+    //     // countriesSection.insertAdjacentHTML('beforeend', independentComponent(countriesJSON[i].independent));
+    //     // countriesSection.insertAdjacentHTML('beforeend', flagComponent(countriesJSON[i].flag));
+    //     console.log(Object.values(countriesJSON[i].flags)[0])
+    // };
+
+    const flagAddButton = document.querySelector('.flag-add');
+    const flagRemoverButton = document.querySelector('.flag-remove');
+
+    const flagInsert = (country, countryInd) => {
+        let divs = [...document.querySelectorAll(".countrycont")];
+        divs.map((element, index) => index === countryInd && element.setAttribute("style", "background-image: url(" + `${Object.values(country.flags)[1]}` + ");background-repeat: no-repeat;background-size: cover; "));
     };
 
-    for (let i = 0; i < countriesJSON.length; i++) {
+    const flagRemove = (country, countryInd) => {
         let divs = [...document.querySelectorAll(".countrycont")];
-        divs.map((x, index) => index === i && x.setAttribute("style", "background-image: url(" + `${Object.values(countriesJSON[i].flags)[1]}` + ");background-repeat: no-repeat;background-size: cover; "));
+        divs.map((element, index) => index === countryInd && element.setAttribute("style", "background-image:none;background-repeat: no-repeat;background-size: cover; "));
     };
+
+    const flagAdder = () => countriesJSON.map((country, index) => flagInsert(country, index));
+    const flagRemover = () => countriesJSON.map((country, index) => flagRemove(country, index));
+
+    flagAddButton.addEventListener('click', flagAdder);
+    flagRemoverButton.addEventListener('click', flagRemover);
+
+    // for (let i = 0; i < countriesJSON.length; i++) {
+    //     let divs = [...document.querySelectorAll(".countrycont")];
+    //     divs.map((x, index) => index === i && x.setAttribute("style", "background-image: url(" + `${Object.values(countriesJSON[i].flags)[1]}` + ");background-repeat: no-repeat;background-size: cover; "));
+    // };
 
     // console.log([...countriesJSON])
 
